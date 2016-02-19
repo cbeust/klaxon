d369dd9087f6d12dd3b368c38650e6e4aa105fe5
Cedric Beust <cedric@beust.com>
2016-02-19 21:41:30 +0400

    0.25.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,18 +1,16 @@
 import com.beust.kobalt.*
-import com.beust.kobalt.plugin.kotlin.kotlinProject
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.java.*
 import com.beust.kobalt.plugin.publish.*
 
-val project = project {
+val p = project {
     name = "klaxon"
     group = "com.beust"
     artifactId = name
     version = "0.25"
 
     dependenciesTest {
-        compile("org.testng:testng:6.9.9")
-        compile("org.jetbrains.kotlin:kotlin-test:1.0.0")
+        compile("org.testng:testng:6.9.9",
+                "org.jetbrains.kotlin:kotlin-test:1.0.0")
     }
 
     assemble {
