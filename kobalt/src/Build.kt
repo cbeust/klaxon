85e9ded0432e9dcf877c0257cb4c661c92e4af27
Ferenc Boldog <ferenc.boldog@gmail.com>
2016-02-29 14:15:59 +0100

    compile with java 1.7 to allow use in android

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,12 +1,17 @@
 import com.beust.kobalt.*
+import com.beust.kobalt.plugin.java.javaCompiler
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.publish.*
+import com.beust.kobalt.plugin.publish.bintray
 
 val p = project {
     name = "klaxon"
     group = "com.beust"
     artifactId = name
-    version = "0.25"
+    version = "0.26"
+
+    javaCompiler {
+        args("-source", "1.7", "-target", "1.7")
+    }
 
     dependenciesTest {
         compile("org.testng:testng:6.9.9",
