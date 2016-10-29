9c05f6ab97e941daacb534c83804c1e72cfaedb0
Cedric Beust <cedric@beust.com>
2016-10-29 08:17:31 -0700

    0.27.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -1,12 +1,13 @@
-import com.beust.kobalt.*
+
 import com.beust.kobalt.plugin.packaging.assemble
-import com.beust.kobalt.plugin.publish.*
+import com.beust.kobalt.plugin.publish.bintray
+import com.beust.kobalt.project
 
 val p = project {
     name = "klaxon"
     group = "com.beust"
     artifactId = name
-    version = "0.26"
+    version = "0.27"
 
     dependenciesTest {
         compile("org.testng:testng:6.9.9",
