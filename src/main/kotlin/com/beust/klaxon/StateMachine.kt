f92e0dab38b429ecf2c87894e49a3b6e53b63415
Cedric Beust <cedric@beust.com>
2018-01-03 14:47:52 -0800

    Refactor.

--- src/main/kotlin/com/beust/klaxon/StateMachine.kt
+++ src/main/kotlin/com/beust/klaxon/StateMachine.kt
@@ -2,7 +2,7 @@ package com.beust.klaxon
 
 private data class TokenStatus(val status: Status, val tokenType: Type)
 
-class StateMachine(val streaming: Boolean) {
+class StateMachine(private val streaming: Boolean) {
     private val map = hashMapOf<TokenStatus, (world: World, token: Token) -> World>()
 
     fun put(status: Status, tokenType: Type, processor: (world: World, token: Token) -> World) {
@@ -11,19 +11,18 @@ class StateMachine(val streaming: Boolean) {
 
     fun next(world: World, token: Token) : World {
         val pair = TokenStatus(world.status, token.tokenType)
-        val processor = map.get(pair)
-        val result = if (processor != null) {
+        val processor = map[pair]
+
+//        println("${status} ${token.tokenType} -> ${world.status}")
+        return if (processor != null) {
             processor(world, token)
         } else {
-            if (! streaming) {
+            if (!streaming) {
                 val message = "No state found: ${world.status} $token"
                 throw KlaxonException(message)
             } else {
                 World(Status.EOF)
             }
         }
-
-//        println("${status} ${token.tokenType} -> ${world.status}")
-        return result
     }
 }
