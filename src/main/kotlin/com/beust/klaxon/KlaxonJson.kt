5e83a05380f093c11971962906e715c492df8220
Cedric Beust <cedric@beust.com>
2018-01-04 15:14:44 -0800

    Move functions around.

--- src/main/kotlin/com/beust/klaxon/KlaxonJson.kt
+++ src/main/kotlin/com/beust/klaxon/KlaxonJson.kt
@@ -3,7 +3,20 @@ package com.beust.klaxon
 import java.lang.IllegalArgumentException
 import java.math.BigInteger
 
+/**
+ * The class used to define the DSL that generates JSON documents. All the functions defined in this class
+ * can be used inside a `json { ... }` call.
+ */
 class KlaxonJson() {
+    fun array(vararg args: Any?) : JsonArray<Any?> = JsonArray(args.map({ convert(it) }))
+
+    fun array(args: List<Any?>) : JsonArray<Any?> = JsonArray(args.map({ convert(it) }))
+
+    // we need this as now JsonArray<T> is List<T>
+    fun <T> array(subArray : JsonArray<T>) : JsonArray<JsonArray<T>> = JsonArray(listOf(subArray))
+
+    fun obj(vararg args: Pair<String, *>): JsonObject =
+            JsonObject(linkedMapOf(*args).mapValues {convert(it.value)})
 
     companion object {
         private fun convert(value: Any?): Any? = when (value) {
@@ -21,15 +34,6 @@ class KlaxonJson() {
         }
     }
 
-    fun array(vararg args: Any?) : JsonArray<Any?> = JsonArray(args.map({ convert(it) }))
-
-    fun array(args: List<Any?>) : JsonArray<Any?> = JsonArray(args.map({ convert(it) }))
-
-    // we need this as now JsonArray<T> is List<T>
-    fun <T> array(subArray : JsonArray<T>) : JsonArray<JsonArray<T>> = JsonArray(listOf(subArray))
-
-    fun obj(vararg args: Pair<String, *>): JsonObject =
-            JsonObject(linkedMapOf(*args).mapValues {convert(it.value)})
 }
 
 /**
