ae5ddecdcf0311f06014c52ccb55a30fc888540b
Cedric Beust <cedric@beust.com>
2018-01-03 18:06:48 -0800

    Better beginObject/Array.

--- src/main/kotlin/com/beust/klaxon/JsonReader.kt
+++ src/main/kotlin/com/beust/klaxon/JsonReader.kt
@@ -13,17 +13,87 @@ class JsonReaderK(val reader: Reader) : Reader() {
 
     val lexer = Lexer(reader)
 
-    fun beginArray() {
+    private fun consumeToken(type: Type, expected: String) {
         val next = lexer.nextToken()
-        if (next.tokenType != Type.LEFT_BRACKET) {
-            throw KlaxonException("Expected a '[' but read $next")
+        if (next.tokenType != type) {
+            throw KlaxonException("Expected a $expected but read $next")
         }
     }
 
-    fun endArray() {
+    private fun privateBeginArray() = consumeToken(Type.LEFT_BRACKET, "[")
+    private fun privateEndArray() = consumeToken(Type.RIGHT_BRACKET, "]")
+
+    private fun privateBeginObject() = consumeToken(Type.LEFT_BRACE, "{")
+    private fun privateEndObject() = consumeToken(Type.RIGHT_BRACE, "}")
+
+    fun nextName(): String {
+        skip()
         val next = lexer.nextToken()
-        if (next.tokenType != Type.RIGHT_BRACKET) {
-            throw KlaxonException("Expected a ']' but read $next")
+        if (next.tokenType == Type.VALUE) {
+            return next.value.toString()
+        } else {
+            throw KlaxonException("Expected a name but got $next")
+        }
+    }
+
+    private val SKIPS = setOf(Type.COLON, Type.COMMA)
+    private fun skip() {
+        while (SKIPS.contains(lexer.peek().tokenType)) lexer.nextToken()
+    }
+
+    private fun <T> consumeValue(convert: (Any?) -> T): T {
+        skip()
+
+        val next = lexer.nextToken()
+        if (next.tokenType == Type.VALUE) {
+            return convert(next.value)
+        } else {
+            throw KlaxonException("Expected a name but got $next")
+        }
+    }
+
+    fun nextString() = consumeValue { value -> value.toString() }
+
+    fun nextInt() = consumeValue { value -> value as Int }
+
+    fun <T> beginObject(closure: () -> T) : T {
+        privateBeginObject()
+        val result = closure()
+        privateEndObject()
+        return result
+    }
+
+    fun <T> beginArray(closure: () -> T) : T {
+        privateBeginArray()
+        val result = closure()
+        privateEndArray()
+        return result
+    }
+
+    fun nextBoolean() = consumeValue { value -> value as Boolean }
+
+    fun nextObject() : JsonObject {
+        skip()
+        return beginObject {
+            val result = JsonObject()
+            while (hasNext()) {
+                val name = nextName()
+                val value = consumeValue { value -> value }
+                result[name] = value
+            }
+            result
+        }
+    }
+
+    fun nextArray() : List<Any> {
+        skip()
+        val result = arrayListOf<Any>()
+        return beginArray {
+            while (hasNext()) {
+                val v = consumeValue { value -> value }
+                result.add(v!!)
+            }
+            result
         }
     }
 
--- src/test/kotlin/com/beust/klaxon/StreamingTest.kt
+++ src/test/kotlin/com/beust/klaxon/StreamingTest.kt
@@ -1,5 +1,6 @@
 package com.beust.klaxon
 
+import com.google.gson.stream.JsonReader
 import org.testng.Assert
 import org.testng.annotations.Test
 import java.io.StringReader
@@ -7,25 +8,67 @@ import java.io.StringReader
 
 @Test
 class StreamingTest {
-    data class Person(var name: String? = null, var age: Int? = null)
+//    data class Person2(var name: String? = null, var age: Int? = null, var flag: Boolean? = false,
+//            var array: List<Int> = emptyList())
+    fun streaming1() {
+        val jr = JsonReader(StringReader(array))
+    }
 
-    fun streaming2() {
-        val array = """[
+    fun streamingObject() {
+        val objectString = """{
+             "name": "Joe", "age": 23, "flag": true, "array": [1, 3],
+             "obj1": { "a":1, "b":2 }
+        }"""
+
+        JsonReaderK(StringReader(objectString)).use { reader ->
+            reader.beginObject() {
+                var name: String? = null
+                var age: Int? = null
+                var flag: Boolean? = null
+                var array: List<Any> = arrayListOf<Any>()
+                var obj1: JsonObject? = null
+                val expectedObj1 = JsonObject().apply {
+                    this["a"] = 1
+                    this["b"] = 2
+                }
+                while (reader.hasNext()) {
+                    val readName = reader.nextName()
+                    when (readName) {
+                        "name" -> name = reader.nextString()
+                        "age" -> age = reader.nextInt()
+                        "flag" -> flag = reader.nextBoolean()
+                        "array" -> array = reader.nextArray()
+                        "obj1" -> obj1 = reader.nextObject()
+                        else -> Assert.fail("Expected either \"name\" or \"age\" but got $name")
+                    }
+                }
+                Assert.assertEquals(name, "Joe")
+                Assert.assertEquals(age, 23)
+                Assert.assertTrue(flag!!)
+                Assert.assertEquals(array, listOf(1, 3))
+                Assert.assertEquals(obj1, expectedObj1)
+            }
+
+        }
+    }
+
+    data class Person1(var name: String? = null, var age: Int? = null)
+    val array = """[
             { "name": "Joe", "age": 23 },
             { "name": "Jill", "age": 35 }
         ]"""
 
+    fun streamingArray() {
         val klaxon = Klaxon()
-
         JsonReaderK(StringReader(array)).use { reader ->
-            val result = arrayListOf<Person>()
-            reader.beginArray()
-            while (reader.hasNext()) {
-                val person = klaxon.parse<Person>(reader)
-                result.add(person!!)
+            val result = arrayListOf<Person1>()
+            reader.beginArray {
+                while (reader.hasNext()) {
+                    val person = klaxon.parse<Person1>(reader)
+                    result.add(person!!)
+                }
+                Assert.assertEquals(result, listOf(Person1("Joe", 23), Person1("Jill", 35)))
             }
-            reader.endArray()
-            Assert.assertEquals(result, listOf(Person("Joe", 23), Person("Jill", 35)))
         }
     }
 
