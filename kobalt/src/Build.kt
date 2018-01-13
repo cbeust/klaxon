f2dc114ca396c3116fdef41891f4fba8cc6cad57
Cedric Beust <cedric@beust.com>
2018-01-12 20:00:51 -0800

    2.0.7.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -9,7 +9,7 @@ val p = project {
     name = "klaxon"
     group = "com.beust"
     artifactId = name
-    version = "2.0.6"
+    version = "2.0.7"
 
     dependencies {
         compile("org.jetbrains.kotlin:kotlin-reflect:1.1.51")
