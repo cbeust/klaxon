26c2eb404419660d13f49ce16b38bbd4f1332a9c
Cedric Beust <cedric@beust.com>
2018-01-20 08:21:55 -0800

    PathObserver.

--- src/main/java/com/beust/klaxon/PathObserver.kt
+++ src/main/java/com/beust/klaxon/PathObserver.kt
@@ -4,5 +4,13 @@ package com.beust.klaxon
  * Invoked whenever the JSON Path encountered matches as a regexp the given path.
  */
 interface PathObserver {
+    /**
+     * @return true if we want to observe this path.
+     */
+    fun pathMatches(path: String) : Boolean
+
+    /**
+     * Note that the value can only be a basic type (Int, String, ...) and never JsonObject or JsonArray.
+     */
     fun onMatch(path: String, value: Any)
 }
