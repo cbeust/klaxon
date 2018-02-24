cbef58ae861d4b768d38e1c5f4c593002258ef67
Cedric Beust <cedric@beust.com>
2018-02-24 06:43:30 -0800

    2.1.7.

--- kobalt/src/Build.kt
+++ kobalt/src/Build.kt
@@ -6,7 +6,7 @@ import com.beust.kobalt.plugin.publish.bintray
 import com.beust.kobalt.project
 
 object Version {
-    val klaxon = "2.1.6"
+    val klaxon = "2.1.7"
     val kotlin = "1.2.10"
 }
 
