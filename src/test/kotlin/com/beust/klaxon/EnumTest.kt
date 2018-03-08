8b7c7709a16f9d6651618401dc048185816a1540
Cedric Beust <cedric@beust.com>
2018-03-07 16:13:26 -0800

    EnumTest.

---
+++ src/test/kotlin/com/beust/klaxon/EnumTest.kt
@@ -0,0 +1,48 @@
+package com.beust.klaxon
+
+import org.testng.Assert
+import org.testng.annotations.Test
+
+@Test
+class EnumTest {
+
+    val convertColor = object: Converter<Color> {
+        override fun toJson(value: Color): String? = when(value) {
+            Color.R -> "red"
+            Color.G -> "green"
+            Color.B -> "blue"
+            else -> null
+        }
+
+        override fun fromJson(jv: JsonValue): Color = when(jv.inside) {
+            "red" -> Color.R
+            "green" -> Color.G
+            "blue" -> Color.B
+            else -> throw IllegalArgumentException("Invalid Color")
+        }
+    }
+
+    enum class Color { R, G, B }
+    data class Root (val colors: List<Color>)
+
+    fun listOfEnums() {
+        val klaxon = Klaxon().converter(convertColor)
+        val result = klaxon.parse<Root>("""
+        {
+            "colors": ["red", "green", "blue"]
+        }
+        """)
+    }
+
+    enum class Cardinal { NORTH, SOUTH }
+    class Direction(var cardinal: Cardinal? = null)
+    fun enum() {
+        val result = Klaxon().parse<Direction>("""
+            { "cardinal": "NORTH" }
+        """
+        )
+        Assert.assertEquals(result?.cardinal, Cardinal.NORTH)
+    }
+
+
+}
