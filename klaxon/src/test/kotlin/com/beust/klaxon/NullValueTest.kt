7e1812d66d26525431b9e60e8555de73beaedd80
Cedric Beust <cedric@beust.com>
2018-11-02 12:03:27 -0700

    Fix test.

--- klaxon/src/test/kotlin/com/beust/klaxon/NullValueTest.kt
+++ klaxon/src/test/kotlin/com/beust/klaxon/NullValueTest.kt
@@ -6,7 +6,7 @@ import org.testng.annotations.Test
 @Test
 class NullValueTest {
     fun nullStrings() {
-        val v = Klaxon().toJsonString(listOf(1, 2, null, null, 3))
-        assertThat(v).isEqualTo("[1, 2, null, null, 3")
+        assertThat(Klaxon().toJsonString(listOf(1, 2, null, null, 3)))
+                .isEqualTo("[1, 2, null, null, 3]")
     }
 }
