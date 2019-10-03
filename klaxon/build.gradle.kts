b279bbb93f62b084cc022f6447523dbcaaaf720c
Cedric Beust <cedric@beust.com>
2019-10-03 09:05:37 -0700

    5.1.

--- klaxon/build.gradle.kts
+++ klaxon/build.gradle.kts
@@ -1,5 +1,5 @@
 object This {
-    val version = "5.0.14-SNAPSHOT"
+    val version = "5.2-SNAPSHOT"
     val groupId = "com.beust"
     val artifactId = "klaxon"
     val description = "A JSON parsing library"
