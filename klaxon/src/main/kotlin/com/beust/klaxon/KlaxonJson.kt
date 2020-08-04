0179fedab54bb9f5824d5008d6f6faedcdcf17e1
Cedric Beust <cedric@beust.com>
2020-08-04 10:48:23 -0700

    New DSL builder
    Fixes https://github.com/cbeust/klaxon/issues/313
    
    This enables more flexible JSON building:
    
        val j = json {
            obj("colors") {
                put("grey", "#333")
                obj("others") {
                    put("white", "#fff")
                }
                put("red", listOf(255, 0, 0))
                put("green", listOf(0, 255, 0))
    
                val someData = Klaxon().toJsonString(""" { "name": "John" }""")
                put("custom_data", someData)
    
                repeat(3) {
                    put("field$it", it * 2)
                }
            }
        }

--- klaxon/src/main/kotlin/com/beust/klaxon/KlaxonJson.kt
+++ klaxon/src/main/kotlin/com/beust/klaxon/KlaxonJson.kt
@@ -1,10 +1,14 @@
 package com.beust.klaxon
 
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.LinkedHashMap
+
 /**
  * The class used to define the DSL that generates JSON documents. All the functions defined in this class
  * can be used inside a `json { ... }` call.
  */
-interface KlaxonJson {
+class KlaxonJson {
     fun array(vararg args: Any?) : JsonArray<Any?> = JsonArray(args.map { convert(it) })
 
     fun array(args: List<Any?>) : JsonArray<Any?> = JsonArray(args.map { convert(it) })
@@ -18,9 +22,24 @@ interface KlaxonJson {
     fun obj(vararg args: Pair<String, *>): JsonObject =
             obj(args.toList())
 
+    fun obj(key: String, init: KlaxonJson.() -> Unit): JsonObject {
+        stackMap.push(LinkedHashMap<String, Any?>())
+        theKlaxonJson.init()
+        val map = stackMap.pop()
+        val newMap = if (stackMap.isEmpty()) HashMap() else stackMap.peek()
+        newMap[key] = JsonObject(map.mapValues { convert(it.value) })
+        return JsonObject(newMap)
+    }
+
+    private val stackMap = Stack<HashMap<String, Any?>>()
+
+    fun put(key: String, value: Any?) {
+        stackMap.peek()[key] = convert(value)
+    }
+
     companion object {
 
-        internal val theKlaxonJson = object : KlaxonJson { }
+        internal val theKlaxonJson = KlaxonJson()
 
         private fun convert(value: Any?): Any? = when (value) {
             is Float -> value.toDouble()
